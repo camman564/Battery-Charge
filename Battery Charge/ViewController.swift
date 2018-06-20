4528c79637785d1b8e43bb0afef4a1e7003443ee
Cameron Swenson <camman564@gmail.com>
2018-06-19 18:54:57 -0700

    Example branch

--- Battery Charge/ViewController.swift
+++ Battery Charge/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let str = "hey look i added some code I want to save"
+        
+        let bigStr = "Hey look... This is a lot of extra stuff, and it might mess up what the project is doing, so im gonna try and do it on a seperate thread"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
