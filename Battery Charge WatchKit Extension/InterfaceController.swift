fd1fcc400dd730f15b5bfcd447d78522cf081c8c
Cameron Swenson <camman564@gmail.com>
2018-06-19 18:32:17 -0700

    Initial Commit

---
+++ Battery Charge WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Battery Charge WatchKit Extension
+//
+//  Created by Cameron Swenson on 6/19/18.
+//  Copyright © 2018 Cameron Swenson. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ Battery Charge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Battery Charge
+//
+//  Created by Cameron Swenson on 6/19/18.
+//  Copyright © 2018 Cameron Swenson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
